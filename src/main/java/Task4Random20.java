37f8b27d2ce98715a8a020e026ef4c10caebdd08
ALut19 <andreylut19@gmail.com>
2017-12-11 16:27:46 +0300

    11.12.2017 Добавлено задание 4

---
+++ src/main/java/Task4Random20.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Андрей Лут on 11.12.2017.
+ */
+
+import java.util.Random;
+import java.util.Arrays;
+
+public class Task4Random20 {
+    public static void main(String[] args) {
+
+        int[] arr = new int[20];
+        fillRandom(arr, 21);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void fillRandom(int[] arr, int n) {
+        int max = 0;
+        int min = 0;
+
+        Random r = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = r.nextInt(n) - 10;
+
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
+    }
+}
